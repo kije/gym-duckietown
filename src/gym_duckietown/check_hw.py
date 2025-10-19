c43c54d76e69cbeb358b14a189ca7468ffb29522
Kim Jeker <github@kije.ch>
2025-10-19 14:54:19 +0200

    Improve headless mode handling in graphics info retrieval
    Refactor get_graphics_information to handle headless mode properly.

--- src/gym_duckietown/check_hw.py
+++ src/gym_duckietown/check_hw.py
@@ -3,28 +3,36 @@ from typing import Dict
 import json
 import pyglet
 
+prev_headless = pyglet.options["headless"]
 pyglet.options["headless"] = True
 from pyglet import gl
+pyglet.options["headless"] = prev_headless
 
 __all__ = ["get_graphics_information"]
 
 
 def get_graphics_information() -> Dict:
-    options = {
-        "vendor": gl.GL_VENDOR,
-        "renderer": gl.GL_RENDERER,
-        "version": gl.GL_VERSION,
-        "shading-language-version": gl.GL_SHADING_LANGUAGE_VERSION,
-        # 'extensions': gl.GL_EXTENSIONS
-    }
-
-    results = {}
-    for o, code in options.items():
-        a = gl.glGetString(code)
-        b: bytes = cast(a, c_char_p).value
-        res = b.decode()
-        results[o] = res
-    return results
+    prev_headless = pyglet.options["headless"]
+    try:
+        pyglet.options["headless"] = True
+        options = {
+            "vendor": gl.GL_VENDOR,
+            "renderer": gl.GL_RENDERER,
+            "version": gl.GL_VERSION,
+            "shading-language-version": gl.GL_SHADING_LANGUAGE_VERSION,
+            # 'extensions': gl.GL_EXTENSIONS
+        }
+    
+        results = {}
+        for o, code in options.items():
+            a = gl.glGetString(code)
+            
+            b: bytes = cast(a, c_char_p).value
+            res = b.decode()
+            results[o] = res
+        return results
+    finally: 
+        pyglet.options["headless"] = prev_headless
 
 
 def main():
