32dc5cb9edeebf29025a5621e03d7c47401ea986
Kim D. Jeker <git@kije.ch>
2025-10-22 11:06:24 +0200

    fix headless

--- src/gym_duckietown/__init__.py
+++ src/gym_duckietown/__init__.py
@@ -16,8 +16,6 @@ on_mac = "Darwin" in platform.system()
 print(pyglet.options)
 if on_mac:
     pyglet.options.headless = False
-else:
-    pyglet.options.headless = True
 
 path = os.path.dirname(os.path.dirname(__file__))
 logger.debug(f"gym-duckietown version {__version__} path {path}\n")
